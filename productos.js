49f6696761d4e65160533c01a73ae4c527b13e5d
Santiago-Henseler <116313638+Santiago-Henseler@users.noreply.github.com>
2025-12-01 00:01:49 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -6,6 +6,7 @@ const tapados = {
     4:  ["Tapado golden bordado tigre", 180000, "", ["imagenes/tapados/4-1.jpeg", "imagenes/tapados/4-2.jpeg", "imagenes/tapados/4-3.jpeg", "imagenes/tapados/4-1.jpeg"], [], [1, 2]],
     5:  ["Tapado metalizado rosa", 160000, "", ["imagenes/tapados/5-1.jpeg", "imagenes/tapados/5-2.jpeg", "imagenes/tapados/5-3.jpeg", "imagenes/tapados/5-1.jpeg"], [], [1, 2]],
     6:  ["Tapado print leopardo", 170000, "", ["imagenes/tapados/6-1.jpeg", "imagenes/tapados/6-2.jpeg", "imagenes/tapados/6-1.jpeg", "imagenes/tapados/6-2.jpeg"], [], [1]],
+    7:  ["Tapado print", 55000, "", ["imagenes/tapados/7-1.jpeg", "imagenes/tapados/7-2.jpeg", "imagenes/tapados/7-3.jpeg", "imagenes/tapados/7-4.jpeg"], [], [1,2,3]],
 }
 
 const camperas = {
@@ -25,6 +26,10 @@ const polleras = {
     4:  ["Falda midi lentejuelas negras", 65000, "", ["imagenes/polleras/4-1.jpeg", "imagenes/polleras/4-2.jpeg", "imagenes/polleras/4-3.jpeg", "imagenes/polleras/4-1.jpeg"], [], [1, 2]],
     5:  ["Falda tul brillos", 85000, "", ["imagenes/polleras/5-1.jpeg", "imagenes/polleras/5-2.jpeg", "imagenes/polleras/5-3.jpeg", "imagenes/polleras/5-1.jpeg"], [], []],
     6:  ["Falda plisada golden", 25000, "", ["imagenes/polleras/6-1.jpeg", "imagenes/polleras/6-2.jpeg", "imagenes/polleras/6-3.jpeg", "imagenes/polleras/6-4.jpeg"], [], [1,2]],
+    7:  ["Falda lentejuelas verde", 35000, "", ["imagenes/polleras/6-1.jpeg", "imagenes/polleras/6-2.jpeg", "imagenes/polleras/6-3.jpeg", "imagenes/polleras/6-4.jpeg"], [], [1,2]],
+    8:  ["Falda lentejuelas midy", 65000, "", ["imagenes/polleras/8-1.jpeg", "imagenes/polleras/8-2.jpeg", "imagenes/polleras/8-3.jpeg", "imagenes/polleras/8-1.jpeg"], ["Negro", "Violeta", "Plata"], []],
+    9:  ["Falda mini violeta", 35000, "", ["imagenes/polleras/9-1.jpeg", "imagenes/polleras/9-2.jpeg", "imagenes/polleras/9-3.jpeg", "imagenes/polleras/9-4.jpeg"], [], [1,2]],
+    10: ["Falda encaje", 25000, "", ["imagenes/polleras/10-1.jpeg", "imagenes/polleras/10-2.jpeg", "imagenes/polleras/10-3.jpeg", "imagenes/polleras/10-4.jpeg"], ["Blanco", "Negro"], [1,2]],
 
 }
 
@@ -35,7 +40,8 @@ const buzos = {
     3:  ["MAXI buzo corazón calado negro", 45000, "Tela rústica", ["imagenes/buzos/3-1.jpeg", "imagenes/buzos/3-2.jpeg", "imagenes/buzos/3-3.jpeg", "imagenes/buzos/3-4.jpeg"], [], [1, 2]],
     4:  ["MAXI buzo Barbie", 35000, "Tela Rústico", ["imagenes/buzos/4-1.jpeg", "imagenes/buzos/4-2.jpeg", "imagenes/buzos/4-3.jpeg", "imagenes/buzos/4-1.jpeg"], [], [1,2,3]],
     5:  ["MAXI buzo corazón calado blanco", 39000, "Tela rústica", ["imagenes/buzos/5-1.jpeg", "imagenes/buzos/5-2.jpeg", "imagenes/buzos/5-3.jpeg", "imagenes/buzos/5-4.jpeg"], [], [1, 2, 3]],
-}
+    6:  ["Buzo guarda bordado lentejuela", 65000, "Tela friza", ["imagenes/buzos/6-1.jpeg", "imagenes/buzos/6-2.jpeg", "imagenes/buzos/6-4.jpeg", "imagenes/buzos/6-4.jpeg"], ["Gris","Negro"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]],
+    7:  ["Buzo I will survive", 55000, "", ["imagenes/buzos/7-1.jpeg", "imagenes/buzos/7-2.jpeg", "imagenes/buzos/7-1.jpeg", "imagenes/buzos/7-2.jpeg"], ["Blanco", "Gris"], [1, 2,]],
 
 const overols = {
     0:0,
@@ -77,6 +83,28 @@ const remeras = {
    11:  ["Musculosa lentejuelones", 45000, "", ["imagenes/remeras/11-1.jpeg", "imagenes/remeras/11-2.jpeg", "imagenes/remeras/11-3.jpeg", "imagenes/remeras/11-4.jpeg"], [], [1,2,3]],
    12:  ["Musculosa acdc", 32000, "", ["imagenes/remeras/12-1.jpeg", "imagenes/remeras/12-2.jpeg", "imagenes/remeras/12-3.jpeg", "imagenes/remeras/12-4.jpeg"], [], [1]],
    13:  ["Remera dúo", 29000, "", ["imagenes/remeras/13-1.jpeg", "imagenes/remeras/13-2.jpeg", "imagenes/remeras/13-3.jpeg", "imagenes/remeras/13-1.jpeg"], [], [1,2]],
+    14: ["Remera Rottweiler", 29000, "", ["imagenes/remeras/14-1.jpeg", "imagenes/remeras/14-2.jpeg", "imagenes/remeras/14-1.jpeg", "imagenes/remeras/14-2.jpeg"], [], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]],
+    15: ["Musculosa Guns and roses", 29000, "", ["imagenes/remeras/15-1.jpeg", "imagenes/remeras/15-2.jpeg", "imagenes/remeras/15-3.jpeg", "imagenes/remeras/15-4.jpeg"], ["Oxido"], ["S","M"]],
+    16: ["Remera Ramones bordada con lentejuelas", 45000, "Lentejuelas blancas perladas", ["imagenes/remeras/16-1.jpeg", "imagenes/remeras/16-2.jpeg", "imagenes/remeras/16-1.jpeg", "imagenes/remeras/16-2.jpeg"], ["Negro"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]],
+    17: ["Remera mariposa bordada con lentejuelas", 55000, "", ["imagenes/remeras/18-1.jpeg", "imagenes/remeras/18-2.jpeg", "imagenes/remeras/18-1.jpeg", "imagenes/remeras/18-2.jpeg"], ["Gris óxido"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]],
+    18: ["Remera red con print", 30000, "", ["imagenes/remeras/17-1.jpeg", "imagenes/remeras/17-2.jpeg", "imagenes/remeras/17-1.jpeg", "imagenes/remeras/17-2.jpeg"], ["Reptil","Leopardo"], []],
+    19: ["Remera deportiva leopardo", 35000, "", ["imagenes/remeras/19-1.jpeg", "imagenes/remeras/19-2.jpeg", "imagenes/remeras/19-3.jpeg", "imagenes/remeras/19-1.jpeg"], [], [1,2]],
+    20: ["Remera The Velvet", 29000, "", ["imagenes/remeras/20-1.jpeg", "imagenes/remeras/20-2.jpeg", "imagenes/remeras/20-1.jpeg", "imagenes/remeras/20-2.jpeg"], ["Negro","Blanco"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]],
+    21: ["Remera Britney", 29000, "", ["imagenes/remeras/21-1.jpeg", "imagenes/remeras/21-2.jpeg", "imagenes/remeras/21-1.jpeg", "imagenes/remeras/21-2.jpeg"], ["Negro","Blanco","Gris","Rojo"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]]
+    22: ["Remera Bambi", 45000, "", ["imagenes/remeras/22-1.jpeg", "imagenes/remeras/22-2.jpeg", "imagenes/remeras/22-1.jpeg", "imagenes/remeras/22-2.jpeg"], ["Negro","Blanco","Beige"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]]
+    23: ["Remera Flor bordado", 45000, "", ["imagenes/remeras/23-1.jpeg", "imagenes/remeras/23-2.jpeg", "imagenes/remeras/23-3.jpeg", "imagenes/remeras/23-1.jpeg"], ["Negro","Blanco"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]]
+    24: ["Remera Betty", 29000, "", ["imagenes/remeras/24-1.jpeg", "imagenes/remeras/24-2.jpeg", "imagenes/remeras/24-3.jpeg", "imagenes/remeras/24-4.jpeg"], ["Oxido"], [1,2]],
+    25: ["Remerón michael Jackson", 35000, "", ["imagenes/remeras/25-1.jpeg", "imagenes/remeras/25-2.jpeg", "imagenes/remeras/25-3.jpeg", "imagenes/remeras/25-1.jpeg"], ["Oxido"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]]
+    26: ["Remera STONE", 45000, "", ["imagenes/remeras/26-1.jpeg", "imagenes/remeras/26-2.jpeg", "imagenes/remeras/26-1.jpeg", "imagenes/remeras/26-2.jpeg"], ["Blanco","Negro","Gris"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]]
+    27: ["Remerón Princess", 29000, "", ["imagenes/remeras/27-1.jpeg", "imagenes/remeras/27-2.jpeg", "imagenes/remeras/27-3.jpeg", "imagenes/remeras/27-4.jpeg"], ["Blanco","Negro","Gris","Rojo","Camel"], ["XS","S","M","L","XL","XXL","XXXL","XXXXL"]]
+    28: ["Vestido rayo", 45000, "", ["imagenes/remeras/28-1.jpeg", "imagenes/remeras/28-2.jpeg", "imagenes/remeras/28-3.jpeg", "imagenes/remeras/28-4.jpeg"], [], [1,2]],
+    29: ["Vestido corazón", 25000, "", ["imagenes/remeras/29-1.jpeg", "imagenes/remeras/29-2.jpeg", "imagenes/remeras/29-3.jpeg", "imagenes/remeras/29-1.jpeg"], [], ["M"]],
+    30: ["Remera lentejuelas cavada", 35000, "", ["imagenes/remeras/30-1.jpeg", "imagenes/remeras/30-2.jpeg", "imagenes/remeras/30-3.jpeg", "imagenes/remeras/30-4.jpeg"], [], []],
+    
+    
+    
+
+
 
 }
 
@@ -84,7 +112,7 @@ const camisas = {
     0:0,
     1:  ["MAXI camisa cómic", 35000, "", ["imagenes/camisas/1-1.jpeg", "imagenes/camisas/1-2.jpeg", "imagenes/camisas/1-3.jpeg", "imagenes/camisas/1-4.jpeg"], [], []],
     2:  ["Camisa playboy", 35000, "Tela poplim elastizado", ["imagenes/camisas/2-1.jpeg", "imagenes/camisas/2-2.jpeg", "imagenes/camisas/2-3.jpeg", "imagenes/camisas/2-1.jpeg"], ["Blanco", "Negro"], []],
-
+    3:  ["MAXI camisa gasa", 45000, "", ["imagenes/camisas/3-1.jpeg", "imagenes/camisas/3-2.jpeg", "imagenes/camisas/3-3.jpeg", "imagenes/camisas/3-1,jpeg"], ["Blanco", "Negro"] []],
 }
 
 const accesorios = {
@@ -99,6 +127,13 @@ const accesorios = {
     8:  ["Cartea nube", 35000, "", ["imagenes/accesorios/8-1.jpeg", "imagenes/accesorios/8-2.jpeg", "imagenes/accesorios/8-3.jpeg", "imagenes/accesorios/8-2.jpeg"], [], []],
     9:  ["Canastas", 15000, "", ["imagenes/accesorios/9-1.jpeg", "imagenes/accesorios/9-2.jpeg", "imagenes/accesorios/9-3.jpeg", "imagenes/accesorios/9-4.jpeg"], [], []],
    10:  ["MAXI cartera Pintada Love", 45000, "Tela eco cuero", ["imagenes/accesorios/10-1.jpeg", "imagenes/accesorios/10-2.jpeg", "imagenes/accesorios/10-3.jpeg", "imagenes/accesorios/10-1.jpeg"], [], []],
+    11: ["Bolsa Lety", 35000, "Tela eco cuero", ["imagenes/Carteras/1-1.jpeg", "imagenes/Carteras/1-2.jpeg", "imagenes/Carteras/1-3.jpeg", "imagenes/Carteras/1-1.jpeg"], ["Fuxia"], []],
+    12: ["Cartera osito marrón", 35000, "Tela peluche", ["imagenes/Carteras/2-1.jpeg", "imagenes/Carteras/2-2.jpeg", "imagenes/Carteras/2-3.jpeg", "imagenes/Carteras/2-4.jpeg"], [], []],
+    13: ["Mini bag con charm", 35000, "", ["imagenes/Carteras/3-1.jpeg", "imagenes/Carteras/3-2.jpeg", "imagenes/Carteras/3-3.jpeg", "imagenes/Carteras/3-4.jpeg"], ["Amarillo", "Negro", "Fuxia"], []],
+    14: ["Sobre kiss", 35000, "", ["imagenes/Carteras/4-1.jpeg", "imagenes/Carteras/4-2.jpeg", "imagenes/Carteras/4-4.jpeg", "imagenes/Carteras/4-5.jpeg"], ["Negro con beso rojo", "Negro con beso violeta"], []],
+    15: ["MAXI bolso", 35000, "", ["imagenes/Carteras/5-1.jpeg", "imagenes/Carteras/5-2.jpeg", "imagenes/Carteras/5-3.jpeg", "imagenes/Carteras/5-1.jpeg"], ["Plata", "Cebra"], []],
+    16: ["Mini bag con Charm matelasse", 29000, "", ["imagenes/Carteras/6-1.jpeg", "imagenes/Carteras/6-2.jpeg", "imagenes/Carteras/6-3.jpeg", "imagenes/Carteras/6-4.jpeg"], ["Plata", "Cebra"], []],
+    17: ["Mini bag Félix ", 35000, "", ["imagenes/Carteras/7-1.jpeg", "imagenes/Carteras/7-2.jpeg", "imagenes/Carteras/7-3.jpeg", "imagenes/Carteras/7-4.jpeg"], ["Cebra animal print", "Rojo", "Plata", "Camel", "Negro con multicolor", "Blanco con multicolor"], []],
 
 }
 
@@ -110,3 +145,32 @@ const cueros = {
     4: ["Campera de flecos", 490000, "", ["imagenes/cueros/4-1.jpeg", "imagenes/cueros/4-2.jpeg", "imagenes/cueros/4-3.jpeg", "imagenes/cueros/4-4.jpeg"], ["Rojo", "Negro"], ["X", "M", "L"]],
     5: ["Gorro", 70000, "", ["imagenes/cueros/5-1.jpeg", "imagenes/cueros/5-2.jpeg", "imagenes/cueros/5-3.jpeg", "imagenes/cueros/4-5.jpeg"], [], []],
 }
+
+
+const collares = {
+    0:0,
+    1: ["MAXI perlas", 35000, "", ["imagenes/collares/1-1.jpeg", "imagenes/collares/1-2.jpeg", "imagenes/collares/1-1.jpeg", "imagenes/collares/1-2.jpeg"], [""], []],
+    2: ["Collar osito", 25000, "", ["imagenes/collares/2-1.jpeg", "imagenes/collares/2-2.jpeg", "imagenes/collares/2-1.jpeg", "imagenes/collares/2-2.jpeg"], [""], []],
+    3: ["Collar multidijes", 35000, "", ["imagenes/collares/3-1.jpeg", "imagenes/collares/3-1.jpeg", "imagenes/collares/3-1.jpeg", "imagenes/collares/3-1.jpeg"], [""], []],
+    4: ["Choker cruz", 25000, "", ["imagenes/collares/4-1.jpeg", "imagenes/collares/4-2.jpeg", "imagenes/collares/4-1.jpeg", "imagenes/collares/4-2.jpeg"], [""], []],
+    5: ["Collar cruz y pinches", 29000, "", ["imagenes/collares/5-1.jpeg", "imagenes/collares/5-2.jpeg", "imagenes/collares/5-1.jpeg", "imagenes/collares/5-2.jpeg"], [""], []],
+    6: ["Collar robot", 29000, "", ["imagenes/collares/6-1.jpeg", "imagenes/collares/6-1.jpeg", "imagenes/collares/6-1.jpeg", "imagenes/collares/6-1.jpeg"], [""], []],
+    7: ["Collar estrellas", 35000, "", ["imagenes/collares/7-1.jpeg", "imagenes/collares/7-1.jpeg", "imagenes/collares/7-1.jpeg", "imagenes/collares/7-1.jpeg"], [""], []],
+    8: ["Choker alfileres", 29000, "", ["imagenes/collares/8-1.jpeg", "imagenes/collares/8-2.jpeg", "imagenes/collares/8-3.jpeg", "imagenes/collares/8-1.jpeg"], [""], []],
+    9: ["Collar moño", 29000, "", ["imagenes/collares/9-1.jpeg", "imagenes/collares/9-2.jpeg", "imagenes/collares/9-1.jpeg", "imagenes/collares/9-2.jpeg"], [""], []],
+    10: ["Collar lluvia", 29000, "", ["imagenes/collares/10-1.jpeg", "imagenes/collares/10-2.jpeg", "imagenes/collares/10-1.jpeg", "imagenes/collares/10-2.jpeg"], [""], []],
+    11: ["Collar lluvia y corazones", 32000, "", ["imagenes/collares/11-1.jpeg", "imagenes/collares/11-2.jpeg", "imagenes/collares/11-1.jpeg", "imagenes/collares/11-2.jpeg"], [""], []],
+    12: ["Collar dijes plata", 25000, "", ["imagenes/collares/12-1.jpeg", "imagenes/collares/12-2.jpeg", "imagenes/collares/12-1.jpeg", "imagenes/collares/12-2.jpeg"], [""], []],
+    13: ["Collar turquesa con pinches", 25000, "", ["imagenes/collares/13-1.jpeg", "imagenes/collares/13-2.jpeg", "imagenes/collares/13-1.jpeg", "imagenes/collares/13-2.jpeg"], [""], []],
+}    
+    
+
+    
+     
+        
+        
+
+
+        
+
+    
